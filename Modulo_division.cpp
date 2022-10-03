4e080859736b9288965fb78f8d128d09922b5af0
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-10-03 09:30:03 +0530

    First Commit in Hashing

---
+++ Modulo_division.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#define SIZE 10
+struct HashTable
+{
+    int *data;
+};
+void insert(int data)
+{
+    
+}
+int main()
+{
+    HashTable hashtab[SIZE];
+
+    return 0;
+}
